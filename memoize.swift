9739339ebaf0556d864253d0644632a5d8245134
Mike Maelzer <mmaelzer@gmail.com>
2014-06-06 15:37:01 -0700

    CONDENSE

--- memoize.swift
+++ memoize.swift
@@ -1,14 +1,10 @@
 func memoize<T:Hashable, U>(fn : T -> U) -> (T -> U) {
     var cache = Dictionary<T, U>()
-
     func memoized(val : T) -> U {
-        if cache.indexForKey(val) {
-            return cache[val]!
-        } else {
-            var valToCache = fn(val)
-            cache[val] = valToCache
-            return valToCache
+        if !cache.indexForKey(val) {
+            cache[val] = fn(val)
         }
+        return cache[val]!
     }
     return memoized;
 }
